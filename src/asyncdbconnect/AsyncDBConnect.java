b91daff66c3f24a2a218442dde1060418c558958
DevInTheShell <DevInTheShell@users.noreply.github.com>
2018-03-06 23:05:45 +0100

    Add AsyncDBConnect Frame.

--- src/asyncdbconnect/AsyncDBConnect.java
+++ src/asyncdbconnect/AsyncDBConnect.java
@@ -4,5 +4,7 @@ public class AsyncDBConnect
 {
     public static void main(String[] args)
     {
+        AsyncDBConnect_Frame frame = new AsyncDBConnect_Frame();
+        frame.setVisible(true);
     }
 }
