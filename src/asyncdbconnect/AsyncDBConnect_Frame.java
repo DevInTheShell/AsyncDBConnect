f7322f808e6a5bff1069c0c6a45157149bc7f351
DevInTheShell <DevInTheShell@users.noreply.github.com>
2018-03-20 17:47:04 +0100

    Improve semantic of source code.

--- src/asyncdbconnect/AsyncDBConnect_Frame.java
+++ src/asyncdbconnect/AsyncDBConnect_Frame.java
@@ -223,6 +223,7 @@ public class AsyncDBConnect_Frame extends javax.swing.JFrame
         ActionListener taskPerformer = new ActionListener()
         {
             int time = 0;
+            
             @Override
             public void actionPerformed(ActionEvent evt)
             {
@@ -272,29 +273,14 @@ public class AsyncDBConnect_Frame extends javax.swing.JFrame
                 }
             }
         }
-        catch (ClassNotFoundException ex)
-        {
-            java.util.logging.Logger.getLogger(AsyncDBConnect_Frame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        catch (InstantiationException ex)
-        {
-            java.util.logging.Logger.getLogger(AsyncDBConnect_Frame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        catch (IllegalAccessException ex)
-        {
-            java.util.logging.Logger.getLogger(AsyncDBConnect_Frame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        catch (javax.swing.UnsupportedLookAndFeelException ex)
+        catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex)
         {
             java.util.logging.Logger.getLogger(AsyncDBConnect_Frame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         
-        java.awt.EventQueue.invokeLater(new Runnable()
+        java.awt.EventQueue.invokeLater(() ->
         {
-            public void run()
-            {
-                new AsyncDBConnect_Frame().setVisible(true);
-            }
+            new AsyncDBConnect_Frame().setVisible(true);
         });
     }
 
